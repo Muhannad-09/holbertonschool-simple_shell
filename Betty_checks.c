c8a0329e100ee6e65201f3beb42ac4b1b855cb7c
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-13 14:03:39 +0000

    Betty checks

---
+++ Betty_checks.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to measure
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
