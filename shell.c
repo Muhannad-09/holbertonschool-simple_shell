7dc56703a58ec41565b66f3a5311345f39787063
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-04-15 15:18:38 +0000

    Simple shell 0.1

--- shell.c
+++ shell.c
@@ -1,84 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>	/* For waitpid */
+#include <string.h>
+#include <sys/wait.h>
 
-extern char **environ;	/* Declare environ to access the environment */
+extern char **environ;
 
-#define BUFFER_SIZE 1024
-
-/**
- * main - simple shell interpreter
- *
- * Return: Always 0.
- */
-int main(void)
+char *trim_spaces(char *str)
 {
-	char *input = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	char *token;
-	char *argv[2];	/* We only need one word commands, so we use size 2 */
-	pid_t pid;
-	int status;
+    char *end;
 
-	/* Infinite loop to keep the shell running */
-	while (1)
-	{
-		/* Display prompt only if input is from terminal (not pipe) */
-		if (isatty(fileno(stdin)))
-		{
-			printf("#cisfun$ ");
-			fflush(stdout);
-		}
+    while (*str == ' ' || *str == '\t')
+        str++;
 
-		/* Read user input */
-		nread = getline(&input, &len, stdin);
+    if (*str == 0)
+        return str;
 
-		/* Handle EOF (Ctrl+D) */
-		if (nread == -1)
-		{
-			printf("\n");
-			break;	/* Exit the shell */
-		}
+    end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t'))
+        end--;
 
-		/* Remove trailing newline character */
-		input[strcspn(input, "\n")] = '\0';
-
-		/* Tokenize the input to get the command */
-		token = strtok(input, " ");
-		if (token != NULL)
-		{
-			/* Prepare the argv array */
-			argv[0] = token;
-			argv[1] = NULL;	/* No arguments, just the command */
-
-			/* Fork a new process to execute the command */
-			pid = fork();
-			if (pid == -1)	/* Error in forking */
-			{
-				perror("Error: ");
-				exit(EXIT_FAILURE);
-			}
-
-			if (pid == 0)	/* Child process */
-			{
-				if (execve(argv[0], argv, environ) == -1)	/* Execute the command */
-				{
-					perror(argv[0]);	/* Command not found */
-					exit(EXIT_FAILURE);
-				}
-			}
-			else	/* Parent process */
-			{
-				waitpid(pid, &status, 0);	/* Wait for the child process to finish */
-			}
-		}
-	}
-
-	/* Clean up and exit */
-	free(input);
-	return (0);
+    *(end + 1) = '\0';
+    return str;
 }
 
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *token, *argv[64];
+    int i;
+    pid_t pid;
+    char *clean_line;
+
+    while (1)
+    {
+        printf("#cisfun$ ");
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            printf("\n");
+            break;
+        }
+
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
+
+        clean_line = trim_spaces(line);
+        if (clean_line[0] == '\0')
+            continue;
+
+        i = 0;
+        token = strtok(clean_line, " \t");
+        while (token != NULL && i < 63)
+        {
+            argv[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+        argv[i] = NULL;
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0)
+        {
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                perror(argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+
+    free(line);
+    return (0);
+}
